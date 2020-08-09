8b52e129141589650ce744029727c8fad4035697
ashish <ashish.1991y@gmail.com>
2020-08-09 19:56:54 +0200

    Port number return

--- src/main/java/com/ashish/springcloud/calculatorapi/application/SimpleCalculatorEndpoints.java
+++ src/main/java/com/ashish/springcloud/calculatorapi/application/SimpleCalculatorEndpoints.java
@@ -1,22 +1,28 @@
 package com.ashish.springcloud.calculatorapi.application;
 
 import com.ashish.springcloud.calculatorapi.exception.SimpleCalculatorException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping(value = "/v1")
 public class SimpleCalculatorEndpoints {
     private final SimpleCalculatorService simpleCalculatorService;
+    @Value("${server.port}")
+    private int portNumber;
 
     SimpleCalculatorEndpoints(final SimpleCalculatorService simpleCalculatorService) {
         this.simpleCalculatorService = simpleCalculatorService;
     }
 
-    @CrossOrigin(origins = "*",allowedHeaders = "*")
-    @PostMapping(value = "/calculate",consumes={"application/json"})
-    public SimpleCalculatorResponseDTO calculateValue(@RequestBody SimpleCalculatorRequestDTO simpleCalculatorRequestDTO) {
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping(value = "/calculate", consumes = {"application/json"})
+    public SimpleCalculatorResponseDTO calculateValue(@RequestBody final SimpleCalculatorRequestDTO simpleCalculatorRequestDTO) {
         final SimpleCalculatorResponseDTO result;
         try {
             result = this.simpleCalculatorService.performOperation(simpleCalculatorRequestDTO);
@@ -27,10 +33,10 @@ public class SimpleCalculatorEndpoints {
         }
         return result;
     }
+
     @GetMapping(value = "/test")
-    public String getTestData()
-    {
-        return "test data";
+    public String getTestData() throws UnknownHostException {
+        return "test data with host details=" + InetAddress.getLocalHost() + " post number=" + this.portNumber;
     }
 }
 
